5ba19129a8ee6fd2d7e76350cc952c20b8975aba
Chris McAnally <chris@adadevelopersacademy.org>
2022-01-20 18:28:04 -0800

    site config file

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Example site',
+    siteUrl: 'https://www.yourdomain.tld',
+    description: 'A sample Gatsby site following the frontendmasters course',
+    image:
+      'https://marketing-media.cloudinary.net/image/upload/c_fill,w_770/dpr_1.0,f_auto,fl_lossy,q_auto/v1/Web_Assets/blog/Gatsby_Video.png',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+};
