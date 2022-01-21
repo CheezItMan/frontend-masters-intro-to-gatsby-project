683bc65837079129507abd213507c4319f1187b3
Chris McAnally <chris@adadevelopersacademy.org>
2022-01-20 19:28:07 -0800

    add mdx and pagecreator config settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,24 @@ module.exports = {
     image:
       'https://marketing-media.cloudinary.net/image/upload/c_fill,w_770/dpr_1.0,f_auto,fl_lossy,q_auto/v1/Web_Assets/blog/Gatsby_Video.png',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-page-creator',
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {},
+    },
+  ],
 };
